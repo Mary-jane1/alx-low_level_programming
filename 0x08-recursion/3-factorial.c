ebf9ec64bbfd2dbcc39fb54ff5ba109b289faeec
Mary-jane1 <ohwokevwo@gmail.com>
2023-05-11 11:52:44 -0700

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * factorial - Calculates the factorial of a number.
+ * @n: The number to calculate the factorial from.
+ *
+ * Return: The factorial of n.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+	/* Negative numbers don't have a factorial, so return -1 */
+	return (-1);
+	}
+	else if (n == 0)
+	{
+	/* The factorial of 0 is defined as 1 */
+	return (1);
+	}
+	else
+	{
+	/* Recursively calculate the factoria */
+	/* l by multiplying n with factorial(n - 1) */
+	return (n * factorial n - 1);
+	}
+}
