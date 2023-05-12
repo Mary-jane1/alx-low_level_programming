cf8b594ecca142887ed910e1ece0090f8682d39d
Mary-jane1 <ohwokevwo@gmail.com>
2023-05-12 11:02:02 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - checks if a string contains only digits
+ * @str: input string
+ *
+ * Return: 1 if the string contains only digits, 0 otherwise
+ */
+int check_num(char *str)
+{
+	unsigned int count = 0;
+
+	while (count < strlen(str))
+	{
+	if (!isdigit(str[count]))
+	{
+	return (0);
+	}
+
+	count++;
+	}
+
+	return (1);
+}
+
+/**
+ * main - Prints the sum of the valid numbers passed as command-line arguments
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+	if (check_num(argv[count]))
+	{
+	str_to_int = atoi(argv[count]);
+	sum += str_to_int;
+	}
+	else
+	{
+	printf("Error\n");
+	return (1);
+	}
+
+	count++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
