a840c776f5ef4a855e217d49122a10724803fe3c
Mary-jane1 <ohwokevwo@gmail.com>
2023-06-02 13:45:13 -0700

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -23,7 +23,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->length = length;
+	new->len = length;
 	new->next = NULL;
 
 	if (*head == NULL)
